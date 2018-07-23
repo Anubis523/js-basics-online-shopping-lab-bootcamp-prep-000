e17d6cfd2a4dfd267dbd25996f770dd7c46994f8
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 13:09:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function viewCart() {
   {
     let msg = "In your cart, you have "
     for (let i = 0; i < cart.length; i++) {
-      if (i < cart.length-1){
+      if (i < cart.length-1 && cart.length > 1){
         msg += `${getCart()[i].itemName} at $${getCart()[i].itemPrice}, `
       }
       else {
