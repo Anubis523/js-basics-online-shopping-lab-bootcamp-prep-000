44ba30d98d6fd05d6225bdc8411ae0879c9497e3
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 13:13:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ function viewCart() {
 
 function total() {
   // write your code here
+  let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+      total += getCart()[i].itemPrice
+    }
+  return total;
 }
 
 function removeFromCart(item) {
