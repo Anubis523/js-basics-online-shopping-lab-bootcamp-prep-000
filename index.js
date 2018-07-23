d7fefd57b06f02329cdaf60cd72c2a5b513bb5e3
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 13:17:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item) {
   // write your code here
     for (let i = 0; i < cart.length; i++) {
       if (item === getCart()[i].itemName) {
-        cart.splice()
+        cart.splice(i,1)
       }
     }
 }
