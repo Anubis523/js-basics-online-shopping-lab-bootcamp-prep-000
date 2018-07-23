505363d203db51d32a9f92f36c940534c9354058
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 13:03:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (cart.length < 1)
+  {
+    return 'Your shopping cart is empty.'
+  }
 }
 
 function total() {
